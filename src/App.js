d18168d7fa3227445da0f0e42cdc3374e6748c71
shreya-8 <shreyap853@gmail.com>
2021-05-25 13:24:49 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import UserComponent from './components/UserComponent';
+
+function App() {
+  return (
+    <div className="App">
+        <UserComponent />
+    </div>
+  );
+}
+
+export default App;
